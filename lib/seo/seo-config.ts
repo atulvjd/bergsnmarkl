2053da8824f1fdb92abab118cbe0d7fc84656b7f
atulvjd <atul.sahu025@gmail.com>
2026-03-03 02:23:16 +0530

    SEO INITIATED

---
+++ lib/seo/seo-config.ts
@@ -0,0 +1,33 @@
+export const SEO_CONFIG = {
+  brandName: "Bergs & Co",
+  alternateNames: ["Bergs & Mark", "Bergs and Mark", "bergsandmark", "berg sand mark"],
+  siteUrl: "https://www.bergsandmark.in",
+  logoUrl: "https://www.bergsandmark.in/icon.svg",
+  location: "Bengaluru",
+  contactEmail: "bergsnmark@gmail.com",
+  contactPhone: "+91 93017 03738",
+  socialLinks: [
+    "https://www.instagram.com/bergsnmark/",
+    "https://www.linkedin.com/company/bergsnmark/",
+    "https://facebook.com",
+  ],
+  defaultOgImage: "/hero-digital-marketing-agency.jpg",
+  coreKeywords: [
+    "marketing agency bengaluru",
+    "marketing agency bangalore",
+    "growth marketing agency india",
+    "bergs and mark",
+    "bergs & mark",
+    "bergsandmark",
+    "berg sand mark",
+    "marketing architecture agency",
+    "growth systems agency",
+    "digital growth infrastructure",
+    "SEO infrastructure",
+  ],
+};
+
+export function getAbsoluteUrl(path: string): string {
+  const normalizedPath = path.startsWith("/") ? path : `/${path}`;
+  return `${SEO_CONFIG.siteUrl}${normalizedPath}`;
+}
