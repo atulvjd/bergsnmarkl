0a180f39ca7429159e51b69dce44879de080796a
atulvjd <atul.sahu025@gmail.com>
2026-03-03 02:25:23 +0530

    SEO INITIATED 5

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,7 +13,7 @@ const _geist = Geist({ subsets: ["latin"] })
 const _geistMono = Geist_Mono({ subsets: ["latin"] })
 
 export const metadata: Metadata = generateMeta({
-  pageType: "home",
+  pageType: "general",
   path: "/",
 });
 
